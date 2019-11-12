df06cac4b513ebd27c35951edb2e5539d473434c
Roger Augusto <rogeraugustosoares@gmail.com>
2019-11-12 16:39:55 -0300

    Reusando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import modelo.dao.DaoFactory;
 import modelo.dao.SellerDao;
-import modelo.entidades.Department;
 import modelo.entidades.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
+		System.out.println(" Teste 1: seller findById ");
 		System.out.println(seller);
 	}
 }
